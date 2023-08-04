f9fd1e171980860746d937210816631336cb02c1
VibhashDwivedi <vibhashdwivedi1005@gmail.com>
2023-08-05 00:01:37 +0530

    homepage

--- Section1/frontend/src/App.jsx
+++ Section1/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './components/Home';
 import Loggedin from './components/Loggedin';
+import Error from './components/Error';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 <Route path='/' element={<Home/>}></Route>
   <Route path='home' element={<Home/>}></Route>
   <Route path='feed' element={<Loggedin/>}></Route>
+  <Route path='error' element={<Error/>}></Route>
 </Routes>
 </BrowserRouter>
 
