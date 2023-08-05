55b63d4799245fd191c3866a676ee78e38cf6ce5
VibhashDwivedi <vibhashdwivedi1005@gmail.com>
2023-08-06 00:09:16 +0530

    backend

---
+++ Section2/backend/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const url ="mongodb+srv://vibhashdwivedi:root@cluster0.bxwasco.mongodb.net/devlinkdb?retryWrites=true&w=majority"
+
+mongoose.connect(url)
+.then((result) => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+module.exports = mongoose;
