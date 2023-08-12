06d02d23b3fd5cfcf8f815fdb419636e09ac1350
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-08-12 08:06:50 +0530

    post

--- Section3/backend/routers/postRouter.js
+++ Section3/backend/routers/postRouter.js
@@ -17,4 +17,18 @@ router.post('/add',(req,res)=>{
     });
 });
 
+router.get('/getall',(req,res)=>{
+
+  Model.find({})
+  .then((result) => {
+      res.json(result);
+  }).catch((err) => {
+      console.log(err);
+      res.status(500).json();
+  });
+});
+
+
+
+
 module.exports = router;
